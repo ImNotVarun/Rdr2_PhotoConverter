4588c5673294888aaf66cb93b4e1fe04ed3fb447
Varun <imnotvarun@gmail.com>
2024-05-08 14:36:30 +0530

    Update Rdr2_Photo_converter.py

--- Rdr2_Photo_converter.py
+++ Rdr2_Photo_converter.py
@@ -1,5 +1,6 @@
 import os
 import errno
+import time
 
 # Get the current working directory
 current_dir = os.getcwd()
@@ -19,16 +20,27 @@ def mkdir_p(path):
 def convert(name):
     in_filename = os.path.join(current_dir, name)
     out_filename = os.path.join("Images", os.path.splitext(name)[0] + '.jpg')
-    print("Converting %s " % in_filename)
+
     with open(in_filename, 'rb') as in_file:
         statinfo = os.stat(in_filename)
         with open(out_filename, 'wb') as out_file:
             out_file.write(in_file.read()[300:])
-    os.utime(out_filename, (statinfo.st_atime, statinfo.st_mtime))
+            os.utime(out_filename, (statinfo.st_atime, statinfo.st_mtime))
+    print(f"Conversion completed")
 
 
 mkdir_p("Images")
 
-for filename in os.listdir(current_dir):
-    if filename.startswith('PRDR'):
+files_to_convert = [filename for filename in os.listdir(
+    current_dir) if filename.startswith('PRDR')]
+total_files = len(files_to_convert)
+
+if total_files > 0:
+    print(f"Found {total_files} files to convert.\n")
+    for i, filename in enumerate(files_to_convert, start=1):
+        print(f"Converting file {i}/{total_files}: {filename}")
         convert(filename)
+    print("\nAll conversions complete.")
+    input("Press Enter to quit...")
+else:
+    print("No files found to convert.")
